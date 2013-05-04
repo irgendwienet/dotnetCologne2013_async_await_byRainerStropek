32e0b1ade03d39863adaea84963f69c0c4869dcd
Benjamin Schröter <benjamin.schroeter@rhein-spree.com>
2013-05-04 10:38:19 +0200

    Moving to an Async API / 1.) the AsyncHeatSensor

---
+++ AsyncAwaitDemo/AsyncAwaitDemo/AsyncHeatSensor.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncAwaitDemo
+{
+    public class AsyncHeatSensor
+    {
+        private bool isConnected = false;
+        private object workInProgressLockObject = new object();
+
+        public Task ConnectAsync(IPAddress address)
+        {
+            // Note that parameters are checked before the task is scheduled.
+            if (address == null)
+            {
+                throw new ArgumentNullException("address");
+            }
+
+            return Task.Run(() =>
+            {
+                // Note that method calls are serialized using this lock statement.
+                // If you want to specify a lock timeout, use Monitor.TryEnter(...)
+                // instead of lock(...).
+                lock (this.workInProgressLockObject)
+                {
+                    if (this.isConnected)
+                    {
+                        throw new InvalidOperationException("Already connected");
+                    }
+
+                    // Simulate connect
+                    Thread.Sleep(3000);
+
+                    this.isConnected = true;
+                }
+            });
+        }
+
+        public Task UploadFirmwareAsync(byte[] firmware, CancellationToken ct, IProgress<int> progress)
+        {
+            if (firmware == null)
+            {
+                throw new ArgumentNullException("firmeware");
+            }
+
+            return Task.Run(() =>
+            {
+                lock (this.workInProgressLockObject)
+                {
+                    if (!this.isConnected)
+                    {
+                        throw new InvalidOperationException("Not connected");
+                    }
+
+                    // Simulate upload in chunks.
+                    for (var i = 1; i <= 10; i++)
+                    {
+                        // Note that we throw an exception if cancellation has been requested.
+                        ct.ThrowIfCancellationRequested();
+
+                        // Simulate uploading of a chunk of data
+                        Thread.Sleep(200);
+
+                        // Report progress
+                        progress.Report(i * 10);
+                    }
+                }
+            }, ct);
+        }
+
+        public Task<bool> TryDisconnectAsync()
+        {
+            return Task.Run(() =>
+            {
+                lock (this.workInProgressLockObject)
+                {
+                    if (!this.isConnected)
+                    {
+                        return false;
+                    }
+
+                    // Simulate disconnect
+                    Thread.Sleep(500);
+
+                    this.isConnected = false;
+                    return true;
+                }
+            });
+        }
+    }
+}
